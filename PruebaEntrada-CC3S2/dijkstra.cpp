b029ee64aae6d40450a7469e91c37fa28afa6d91
CesarSanchez04 <cesar.asm2004@gmail.com>
2024-09-02 15:18:12 -0500

    Documentacion del codigo (completa)

--- PruebaEntrada-CC3S2/dijkstra.cpp
+++ PruebaEntrada-CC3S2/dijkstra.cpp
@@ -4,6 +4,8 @@
 #include <limits>
 using namespace std;
 
+// Esta es una implementación del algoritmo de Dijkstra para encontrar el camino más corto en un grafo dirigido
+
 #define MAX 10005
 #define INF numeric_limits<int>::max()
 
@@ -20,6 +22,10 @@ int distancia[MAX];
 int previo[MAX];
 bool visitado[MAX];
 
+// se define las variables con sus respectivos valores
+// las distancias se inicializan con valores infinitos 
+// la lista visitado incializa todo en falso ya que al inicio del algoritmo ningun nodo fue visitado aun
+// la lista previo inicia en -1 ya que no hay nodo previo al inicio
 void init() {
 	for (int i = 0; i < MAX; ++i) {
 		distancia[i] = INF;
@@ -28,6 +34,8 @@ void init() {
 	}
 }
 
+// se define la función que realiza la minimización de las distancias
+// esto logra que en el caso haya un camino aun mejor al conseguido se cambia el camino optimo
 void minimizacion(int actual, int adyacente, int peso) {
 	if (distancia[actual] + peso < distancia[adyacente]) {
 		distancia[adyacente] = distancia[actual] + peso;
@@ -43,17 +51,24 @@ void print(int destino) {
 
 void dijkstra(int inicial) {
 	init();
+
+	// se crea una cola de prioridad y se encola el nodo inicial con distancia 0
 	priority_queue<Nodo, vector<Nodo>, Compare> Q;
 	distancia[inicial] = 0;
 	Q.push(Nodo(inicial, 0));
-	
+
+	// mientras la cola de prioridad no ese vacia el bucle continua 
 	while (!Q.empty()) {
+		// se elige al primero de la cola de prioridad
 		int actual = Q.top().first;
+		// se borra al primero de la cola de prioridad luego de copiarlo a la variable actual
 		Q.pop();
-		
+
+		// si el nodo ya ha sido visitado se sigue al siguiente nodo
 		if (visitado[actual]) continue;
 		visitado[actual] = true;
 		
+		// se itera sobre los nodos adyacentes al nodo actual y se minimiza la distancia si es necesario
 		for (const auto& nodo : ady[actual]) {
 			int adyacente = nodo.first;
 			int peso = nodo.second;
@@ -80,6 +95,9 @@ void dijkstra(int inicial) {
 }
 
 int main() {
+	// se instancia las variables iniciales para el algoritmo
+	// V = cantidad de nodos , E = cantidad de aristas , origen = nodo inicial , destino = nodo final, peso = distancia o peso a tratar entre los
+	// nodos (origen y destino) , inicial = vertice donde se inicia el algoritmo
 	int V, E, origen, destino, peso, inicial;
 	
 	cout << "\tBienvenido al programa de Grafos para hallar la distancia mas corta de un nodo o vertice al otro." << endl;
@@ -89,6 +107,8 @@ int main() {
 	cout << "\tIngrese el numero de arcos: ";
 	cin >> E;
 	
+	// Se procede a solicitar las aristas en base a la cantidad de nodos y aristas elegidas por el usuario
+	// Se añaden las aristas usando colas
 	cout << "\tIngresar el nodo o vertice de origen, su arista y la distancia o peso de un nodo a otro dejando un espacio entre cada numero." << endl;
 	for (int i = 0; i < E; i++) {
 		cin >> origen >> destino >> peso;
